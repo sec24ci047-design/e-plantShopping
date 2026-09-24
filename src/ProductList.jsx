b6eecac666d4ee17a7d5360d38d7bec8fcb6404e
sec24ci047-design <sec24ci047@sairamtap.edu.in>
2026-09-24 21:28:18 +0530

    Refactor ProductList component and improve styling

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import './ProductList.css'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './ProductList.css';
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showPlants, setShowPlants] = useState(false);
+
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.items);
 
     const plantsArray = [
         {
@@ -52,13 +58,13 @@ function ProductList({ onHomeClick }) {
             plants: [
                 {
                     name: "Lavender",
-                    image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop",
                     description: "Calming scent, used in aromatherapy.",
                     cost: "$20"
                 },
                 {
                     name: "Jasmine",
-                    image: "https://images.unsplash.com/photo-1592729645009-b96d1e63d14b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    image: "https://images.unsplash.com/photo-1592729645009-b96d1e63d14b?q=80&w=1170&auto=format&fit=crop",
                     description: "Sweet fragrance, promotes relaxation.",
                     cost: "$18"
                 },
@@ -83,7 +89,7 @@ function ProductList({ onHomeClick }) {
                 {
                     name: "Hyacinth",
                     image: "https://cdn.pixabay.com/photo/2019/04/07/20/20/hyacinth-4110726_1280.jpg",
-                    description: "Hyacinth is a beautiful flowering plant known for its fragrant.",
+                    description: "Beautiful flowering plant known for its fragrance.",
                     cost: "$22"
                 }
             ]
@@ -92,32 +98,32 @@ function ProductList({ onHomeClick }) {
             category: "Insect Repellent Plants",
             plants: [
                 {
-                    name: "oregano",
+                    name: "Oregano",
                     image: "https://cdn.pixabay.com/photo/2015/05/30/21/20/oregano-790702_1280.jpg",
-                    description: "The oregano plants contains compounds that can deter certain insects.",
+                    description: "Contains compounds that can deter certain insects.",
                     cost: "$10"
                 },
                 {
                     name: "Marigold",
                     image: "https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
-                    description: "Natural insect repellent, also adds color to the garden.",
+                    description: "Natural insect repellent and adds color to the garden.",
                     cost: "$8"
                 },
                 {
                     name: "Geraniums",
                     image: "https://cdn.pixabay.com/photo/2012/04/26/21/51/flowerpot-43270_1280.jpg",
-                    description: "Known for their insect-repelling properties while adding a pleasant scent.",
+                    description: "Known for insect-repelling properties.",
                     cost: "$20"
                 },
                 {
                     name: "Basil",
                     image: "https://cdn.pixabay.com/photo/2016/07/24/20/48/tulsi-1539181_1280.jpg",
-                    description: "Repels flies and mosquitoes, also used in cooking.",
+                    description: "Repels flies and mosquitoes.",
                     cost: "$9"
                 },
                 {
                     name: "Lavender",
-                    image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop",
                     description: "Calming scent, used in aromatherapy.",
                     cost: "$20"
                 },
@@ -141,7 +147,7 @@ function ProductList({ onHomeClick }) {
                 {
                     name: "Echinacea",
                     image: "https://cdn.pixabay.com/photo/2014/12/05/03/53/echinacea-557477_1280.jpg",
-                    description: "Boosts immune system, helps fight colds.",
+                    description: "Boosts immune system and helps fight colds.",
                     cost: "$16"
                 },
                 {
@@ -175,7 +181,7 @@ function ProductList({ onHomeClick }) {
             plants: [
                 {
                     name: "ZZ Plant",
-                    image: "https://images.unsplash.com/photo-1632207691143-643e2a9a9361?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    image: "https://images.unsplash.com/photo-1632207691143-643e2a9a9361?q=80&w=464&auto=format&fit=crop",
                     description: "Thrives in low light and requires minimal watering.",
                     cost: "$25"
                 },
@@ -212,26 +218,6 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
-    const styleObj = {
-        backgroundColor: '#4CAF50',
-        color: '#fff!important',
-        padding: '15px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignIems: 'center',
-        fontSize: '20px',
-    }
-    const styleObjUl = {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '1100px',
-    }
-    const styleA = {
-        color: 'white',
-        fontSize: '30px',
-        textDecoration: 'none',
-    }
 
     const handleHomeClick = (e) => {
         e.preventDefault();
@@ -240,45 +226,133 @@ function ProductList({ onHomeClick }) {
 
     const handleCartClick = (e) => {
         e.preventDefault();
-        setShowCart(true); // Set showCart to true when cart icon is clicked
+        setShowCart(true);
     };
+
     const handlePlantsClick = (e) => {
         e.preventDefault();
-        setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
-        setShowCart(false); // Hide the cart when navigating to About Us
+        setShowPlants(true);
+        setShowCart(false);
     };
 
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const handleAddToCart = (plant) => {
+        dispatch(addItem(plant));
+    };
+
+    const getCartQuantity = () => {
+        return cart.reduce((total, item) => total + item.quantity, 0);
+    };
+
     return (
         <div>
-            <div className="navbar" style={styleObj}>
+            <div className="navbar">
                 <div className="tag">
                     <div className="luxury">
-                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/" onClick={(e) => handleHomeClick(e)}>
+                        <img
+                            src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png"
+                            alt="Paradise Nursery"
+                        />
+
+                        <a href="/" onClick={handleHomeClick}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                                <h3 style={{ color: 'white' }}>
+                                    Paradise Nursery
+                                </h3>
+                                <i style={{ color: 'white' }}>
+                                    Where Green Meets Serenity
+                                </i>
                             </div>
                         </a>
                     </div>
-
                 </div>
-                <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+
+                <div className="ul">
+                    <div>
+                        <a href="#" onClick={handlePlantsClick}>
+                            Plants
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="#" onClick={handleCartClick}>
+                            <h1 className="cart">
+                                🛒
+                                <span className="cart_quantity_count">
+                                    {getCartQuantity()}
+                                </span>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
+
             {!showCart ? (
                 <div className="product-grid">
+                    {plantsArray.map((category) => (
+                        <div key={category.category}>
+                            <div className="plantname_heading">
+                                <h2 className="plant_heading">
+                                    {category.category}
+                                </h2>
+                            </div>
 
+                            <div className="product-list">
+                                {category.plants.map((plant) => {
+                                    const isAdded = cart.some(
+                                        (item) => item.name === plant.name
+                                    );
 
+                                    return (
+                                        <div
+                                            className="product-card"
+                                            key={`${category.category}-${plant.name}`}
+                                        >
+                                            <img
+                                                className="product-image"
+                                                src={plant.image}
+                                                alt={plant.name}
+                                            />
+
+                                            <h3 className="product-title">
+                                                {plant.name}
+                                            </h3>
+
+                                            <p>{plant.description}</p>
+
+                                            <div className="product-price">
+                                                {plant.cost}
+                                            </div>
+
+                                            <button
+                                                className={`product-button ${
+                                                    isAdded
+                                                        ? 'added-to-cart'
+                                                        : ''
+                                                }`}
+                                                onClick={() =>
+                                                    handleAddToCart(plant)
+                                                }
+                                            >
+                                                {isAdded
+                                                    ? 'Added to Cart'
+                                                    : 'Add to Cart'}
+                                            </button>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             ) : (
-                <CartItem onContinueShopping={handleContinueShopping} />
+                <CartItem
+                    onContinueShopping={handleContinueShopping}
+                />
             )}
         </div>
     );
